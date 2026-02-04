561e25499ce5b686632d071ba40ec0fcb682c8a9
HITLUK <kirich721005@gmail.com>
2026-02-04 11:30:32 +0300

    task 1+2

---
+++ ru/hse/lab1/second/Person.java
@@ -0,0 +1,16 @@
+package ru.hse.lab1.second;
+
+public class Person {
+    String name = "";
+    int age = 0;
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+public String getName() {
+    return name;
+}
+public int getAge() {
+    return age;
+}
+}
